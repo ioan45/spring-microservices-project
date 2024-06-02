5588712247965e152f7df0c0ff8757109d290cc1
ioan45 <alexandruculea45@gmail.com>
2024-06-02 21:51:50 +0300

    Added first microservices

---
+++ microservices/spring-cloud-config-microservice/src/main/java/com/example/springcloudconfigmicroservice/SpringCloudConfigMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.springcloudconfigmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigMicroserviceApplication.class, args);
+    }
+
+}
---
+++ microservices/spring-cloud-config-microservice/src/test/java/com/example/springcloudconfigmicroservice/SpringCloudConfigMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
