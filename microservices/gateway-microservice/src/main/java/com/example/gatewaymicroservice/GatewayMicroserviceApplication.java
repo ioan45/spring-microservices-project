93420475f4b72bbe103b49dce98d9100df507c07
ioan45 <alexandruculea45@gmail.com>
2024-06-02 23:23:22 +0300

    Added gateway & load balancer & eureka

--- microservices/gateway-microservice/src/main/java/com/example/gatewaymicroservice/GatewayMicroserviceApplication.java
+++ microservices/gateway-microservice/src/main/java/com/example/gatewaymicroservice/GatewayMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.postmicroservice;
+package com.example.gatewaymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PostMicroserviceApplication {
+public class GatewayMicroserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostMicroserviceApplication.class, args);
+        SpringApplication.run(GatewayMicroserviceApplication.class, args);
     }
 
 }
